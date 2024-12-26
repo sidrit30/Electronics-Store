ebdb00eb3177432b6166436ebc8116b35429ce2f
Sidrit Zela <szela23@epoka.edu.al>
2024-12-27 00:11:46 +0100

    Set up

--- src/main/java/com/cen215/electronicsstore/HelloApplication.java
+++ src/main/java/com/cen215/electronicsstore/HelloApplication.java
@@ -1,17 +1,17 @@
 package com.cen215.electronicsstore;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+    public void start(Stage stage) {
+
+        Scene scene = new Scene(new Pane());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
