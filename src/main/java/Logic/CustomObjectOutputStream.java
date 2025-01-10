1acab74f6fefa43d93599a0e33833c4e509d2927
Sidrit Zela <szela23@epoka.edu.al>
2025-01-10 12:09:34 +0100

    Orginazation of directories

---
+++ src/main/java/Logic/CustomObjectOutputStream.java
@@ -0,0 +1,16 @@
+package Logic;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+public class CustomObjectOutputStream extends ObjectOutputStream {
+    public CustomObjectOutputStream(OutputStream out) throws IOException {
+        super(out);
+    }
+
+    @Override
+    protected void writeStreamHeader() throws IOException {
+
+    }
+}
--- src/main/java/View/LoginPage.java
+++ src/main/java/View/LoginPage.java
@@ -8,12 +8,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.util.Objects;
 
 public class LoginPage{
     Stage primaryStage;
